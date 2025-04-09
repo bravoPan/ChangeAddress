211ad877c2b6a8acfb0b32671c91a8f4b719df87
dengpan.yuan <dengpan.yuan@bytedance.com>
2025-04-08 17:26:03 -0700

    push in repo

--- CheckAddress/Controller/AddAddressViewController.swift
+++ CheckAddress/Controller/AddAddressViewController.swift
@@ -1,5 +1,18 @@
+//
+//  AddAddressViewController.swift
+//  CheckAddress
+//
+//  Created by ByteDance on 4/8/25.
+//  Copyright © 2025 MAC. All rights reserved.
+//
+
 // AddAddressViewController.swift
 import UIKit
+import Foundation
+
+protocol AddAddressDelegate: AnyObject {
+    func didAddAddress(_ address: Address)
+}
 
 class AddAddressViewController: UIViewController {
     
@@ -10,22 +23,24 @@ class AddAddressViewController: UIViewController {
 
     weak var delegate: AddAddressDelegate?
 
-    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
-        // Validate input
-        guard let street = streetTextField.text,
-              let city = cityTextField.text,
-              let state = stateTextField.text,
-              let zipCode = zipCodeTextField.text else {
-            return
-        }
-
-        // Create Address object
-        let newAddress = Address(street: street, city: city, state: state, zipCode: zipCode)
-
-        // Pass the new address back to the previous view controller
-        delegate?.didAddAddress(newAddress)
-
-        // Dismiss the view controller
-        dismiss(animated: true, completion: nil)
+    @IBAction func s(_ sender: Any) {
     }
+    @IBAction func doneButtonTapped(_ sender: UIViewController) {
+            // Validate input
+            guard let street = streetTextField.text,
+                  let city = cityTextField.text,
+                  let state = stateTextField.text,
+                  let zipCode = zipCodeTextField.text else {
+                return
+            }
+
+            // Create Address object
+            let newAddress = Address(street: street, city: city, state: state, zipCode: zipCode)
+
+            // Pass the new address back to the previous view controller
+            delegate?.didAddAddress(newAddress)
+
+            // Dismiss the view controller
+            dismiss(animated: true, completion: nil)
+        }
 }
